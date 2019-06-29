702a646821110f1117c713d40fb12a8b3315a863
ByronStewart <41535561+ByronStewart@users.noreply.github.com>
2019-06-29 11:29:22 +1000

    added minified js

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,11 @@
-module.exports = {
+import minify from "rollup-plugin-babel-minify";
+
+export default {
   input: "src/main.js",
+
   output: {
     file: "dist/bundle.js",
     format: "esm"
-  }
+  },
+  plugins: [minify()]
 };
